6356c2166b8d1f1cb0b4d0c9344a269e2929f9fa
Ana Rosario <arosario.mrd@gmail.com>
2022-12-26 21:35:11 -0500

    commit tree 73b90ad08c5768aba59112d3fbcfd06e4494289b

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
-# Your code here!
+# Define a method #greet_programmer that takes no arguments. It should output the string "Hello, programmer!" to the terminal with #puts.
+
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1,num2)
+    return num1 + num2
+end
+
+def halve(num)
+    return nil unless num.class == Integer
+
+    num / 2
+end
